a3f5bee16ee2648c1d608e63915baed93a7120ad
Gerardo Gabriel Caldas Díaz <u201512034@upc.edu.pe>
2021-10-02 07:47:33 -0500

    mycases
    Realization of the web application with visualization of lawyers and my cases made

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CasesComponent } from "./cases/pages/cases/cases.component";
+import { HomeComponent } from "./public/home/home.component";
+import { AboutComponent } from "./public/about/about.component";
+import { LayersComponent } from "./layers/pages/layers/layers.component";
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'layers', component: LayersComponent},
+  { path: 'cases', component: CasesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { CasesComponent } from './cases/pages/cases/cases.component';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { HomeComponent } from './public/home/home.component';
+import { AboutComponent } from './public/about/about.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { LayersComponent } from './layers/pages/layers/layers.component';
+import {MatSortModule} from "@angular/material/sort";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CasesComponent,
+    HomeComponent,
+    AboutComponent,
+    LayersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSortModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
